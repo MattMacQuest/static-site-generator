772dabf30200d7f1b204099e0b354bf7914ad52e
Matt MacQueston <Matt.MacQuest@gmail.com>
2026-03-06 14:50:33 -0800

    Initial Commit. Framework for static site generator set with some basic test cases.

---
+++ src/htmlnode.py
@@ -0,0 +1,15 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.propr = props
+    
+    def to_html(self):
+        raise NotImplementedError()
+    
+    def props_to_html(self):
+        pass
+    
+    def __repr__(self):
+        pass
---
+++ src/test_htmlnode.py
@@ -0,0 +1,8 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
