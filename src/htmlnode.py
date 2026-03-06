dce8870fccd5b1468fbcf2c0f36965dd139bb164
Matt MacQueston <Matt.MacQuest@gmail.com>
2026-03-06 15:34:33 -0800

    Added HTMLNode class and associated test file.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -3,13 +3,34 @@ class HTMLNode():
         self.tag = tag
         self.value = value
         self.children = children
-        self.propr = props
+        self.props = props
     
     def to_html(self):
         raise NotImplementedError()
     
     def props_to_html(self):
-        pass
+        prop_list = []
+        for key, value in self.props.items():
+            prop_list.append(f" {key}=\"{value}\"")
+        
+        prop_string = "".join(prop_list)
+        return prop_string
+    
+    def __eq__(self, other):
+        return (
+            self.tag == other.tag
+            and self.value == other.value
+            and self.children == other.children
+            and self.props == other.props
+        )
+    
+    def __ne__(self, other):
+        return (
+            self.tag != other.tag
+            or self.value != other.value
+            or self.children != other.children
+            or self.props != other.props
+        )
     
     def __repr__(self):
-        pass
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,7 +2,39 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    pass
+    def test_ne(self):
+        node = HTMLNode("a", "googler", None, None)
+        node2 = HTMLNode("a", "google", None, None)
+        self.assertNotEqual(node, node2)
+    
+    def test_eq(self):
+        node = HTMLNode("a", "google", None, None)
+        node2 = HTMLNode("a", "google", None, None)
+        self.assertEqual(node, node2)
+    
+    def test_props_to_html(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+    
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
 
 if __name__ == "__main__":
     unittest.main()
