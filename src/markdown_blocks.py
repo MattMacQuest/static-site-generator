75384260a2bae875a59bf4385b4eca7e6d709dba
MattMacQuest <Matt.MacQuest@gmail.com>
2026-03-18 14:42:00 -0700

    Added functionality to identify block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -8,7 +8,7 @@ class BlockType(Enum):
     UNORDERED_LIST = "unordered_list"
     ORDERED_LIST = "ordered_list"
 
-def markdown_to_blocks(markdown: str):
+def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     stripped_blocks = [block.strip() for block in blocks]
     
@@ -16,5 +16,31 @@ def markdown_to_blocks(markdown: str):
     
     return stripped_blocks
 
-def block_to_block_type(markdown: str):
-    pass
+def block_to_block_type(block: str) -> BlockType:
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    lines = block.split("\n")
+    
+    
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    elif block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type
 
 class TestMarkdownBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -43,6 +43,88 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+        
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        
+    def test_markdown_to_blocks_to_block_type_simple(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        block_list = markdown_to_blocks(md)
+        block_types = []
+        for block in block_list:
+            block_types.append(block_to_block_type(block))
+        
+        self.assertListEqual(
+            [
+                BlockType.PARAGRAPH,
+                BlockType.PARAGRAPH,
+                BlockType.UNORDERED_LIST
+            ],
+            block_types
+        )
+        
+    def test_markdown_to_blocks_to_block_type_simple(self):
+        md = """
+This is **bolded** paragraph
+
+1. This
+2. Is
+3. A
+4. Numbered
+5. List
+
+> With some
+> quotes from
+> Mike
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+
+```
+and finally some code
+```
+"""
+        block_list = markdown_to_blocks(md)
+        block_types = []
+        for block in block_list:
+            block_types.append(block_to_block_type(block))
+        
+        self.assertListEqual(
+            [
+                BlockType.PARAGRAPH,
+                BlockType.ORDERED_LIST,
+                BlockType.QUOTE,
+                BlockType.PARAGRAPH,
+                BlockType.UNORDERED_LIST,
+                BlockType.CODE
+            ],
+            block_types
+        )
+        
         
 if __name__ == "__main__":
     unittest.main()
