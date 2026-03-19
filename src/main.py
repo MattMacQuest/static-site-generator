0301864c6e7dc0a7cae143274a54f10749e2b6de
MattMacQuest <Matt.MacQuest@gmail.com>
2026-03-19 15:07:11 -0700

    Added index.css

--- src/main.py
+++ src/main.py
@@ -1,18 +1,7 @@
-from textnode import TextNode, TextType, text_node_to_html_node
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
-from markdown_blocks import markdown_to_html_node
-from enum import Enum
+import os, shutil
 
 def main():
-    md = """
-```
-This is text that _should_ remain
-the **same** even with inline stuff
-```
-"""
-    markdown_to_html_node(md)
-    # print(node)
-    # print(new_nodes)
+    pass
 
 if __name__ == "__main__":
     main()
